50a6200fb6749377c824c408c47f850a8af1010f
Christopher Amanor <kriskit7@gmail.com>
2020-08-05 10:08:16 +0000

    Add startGame function

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,7 +1,8 @@
 const boardDisplay = document.querySelector('.board-display');
 const cells = document.querySelectorAll('.cell');
-const playerSym = document.querySelector('#symbol');
 const playerName = document.querySelector('#username');
 const form = document.querySelector('.form');
 
-export { boardDisplay, cells, playerName, playerSym, form };
+export {
+  boardDisplay, cells, playerName, form,
+};
--- scripts/index.js
+++ scripts/index.js
@@ -1,73 +1,87 @@
-import { boardDisplay, cells, playerName, playerSym, form } from './dom.js';
+import {
+  boardDisplay, cells, playerName, form,
+// eslint-disable-next-line import/extensions
+} from './dom.js';
 
-let gameBoard = {
-   board: ['', '', '', '', '', '', '', '', '']
-}
+const gameBoard = {
+  board: ['', '', '', '', '', '', '', '', ''],
+};
 
-console.log(gameBoard.board);
+const playerOneSym = 'X';
+const playerTwoSym = 'O';
+const playersArr = [];
+
+// console.log(gameBoard.board);
 // // let gameBoard = (() => {
 // //   let board = ['_', 'O', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
 // //   return { board };
 // // })();
 
 
- const gameStart = () => {
-   
- }
+const gameStart = () => {};
 
-const updateCell = ( gameBoard ) => {
-  const {index} = event.target.dataset
-  let gameArr = gameBoard;
+const updateCell = (gameBoard) => {
+  const { index } = event.target.dataset;
+  const gameArr = gameBoard;
   const playerInput = index;
   console.log(event.target.dataset);
-  gameArr[index-1] = 'O'
+  gameArr[index - 1] = 'O';
   render(cells, gameBoard, index);
-}
-
-
-let player = (name, symbol) => {
-  return { name, symbol };
 };
 
 
-let createPlayer = (name, symbol) => {
-const playerOne =  player(name.value, symbol);
-// const playerTwo =  player(name.value, 'O')
-}
-
-let gameFlow = () => {
-  alert('Player One enter your name:');
-  createPlayer(playerName, 'X');
-  alert('Player Two enter your name:');
-  createPlayer(playerName, 'O');
+// const player = (name, symbol) => ({ name, symbol });
 
 
-  gameBoard.board.forEach (element => {
-    if(element !== ''){
-      
-    }
-  })
+const createPlayer = (name, symbol) => {
+  return { name, symbol };
+};
+
+const gameFlow = (playersArr) => {
+  // alert('Player One enter your name:');
+  console.log(playersArr);
+  const playerOne = createPlayer(playersArr[0], playerOneSym);
+  const playerTwo = createPlayer(playersArr[1], playerTwoSym);
+  // alert('Player Two enter your name:');
+  // createPlayer(playerName, 'O');
+  console.log(playerOne);
+  console.log(playerTwo);
+
+
+  // gameBoard.board.forEach(element => {
+  //   if (element !== '') {
+
+  //   }
+  // });
 
 
   // players take turn
   // player input array
   // wins combos
-  // 
+  //
 };
 
-gameFlow();
+// gameFlow();
 
 const render = (cells, gameBoard, index) => {
-
-  cells[parseInt(index)-1].innerHTML = gameBoard.board[index-1]
- 
+  cells[parseInt(index) - 1].innerHTML = gameBoard.board[index - 1];
 };
 
 // // console.log(gameBoard);
 // // console.log(gameBoard.game());
 
+const startGame = (e) => {
+  e.preventDefault();
+  // console.log(playerName.value);
+  playersArr.push(playerName.value);
+  // console.log(playersArr);
+
+  if (playersArr.length === 2) {
+    gameFlow(playersArr);
+  }
+  form.reset();
+};
 
 
-
-[].forEach.call(cells,function(e){e.addEventListener('click',()=>{updateCell(gameBoard)} , false)}) 
-form.addEventListener('submit', createPlayer);
+[].forEach.call(cells, (e) => { e.addEventListener('click', () => { updateCell(gameBoard); }, false); });
+form.addEventListener('submit', (e) => startGame(e));
