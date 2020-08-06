6449dce24806c53c4f47a6c27eefc732edfcc9c6
Christopher Amanor <kriskit7@gmail.com>
2020-08-06 09:21:21 +0000

    Refactor code

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,10 @@ import {
 // eslint-disable-next-line import/extensions
 } from './dom.js';
 
+const playerOneSym = 'X';
+const playerTwoSym = 'O';
+const playersArr = [];
+const playersObj = [];
 const gameBoard = {
   board: ['', '', '', '', '', '', '', '', ''],
 };
@@ -18,24 +22,7 @@ const winCombos = [
   [2, 4, 6],
 ];
 
-const playerOneSym = 'X';
-const playerTwoSym = 'O';
-const playersArr = [];
-const playersObj = [];
-
-const checkWin = (winCombos, symbol, gameBoard) => {
-  console.log(winCombos);
-  for (let i = 0; i < winCombos.length; i += 1) {
-    const a = gameBoard.board[winCombos[i][0]];
-    const b = gameBoard.board[winCombos[i][1]];
-    const c = gameBoard.board[winCombos[i][2]];
-
-    if (a === symbol && b === symbol && c === symbol) {
-      console.log('it works!');
-      return true;
-    }
-  }
-};
+const createPlayer = (name, symbol) => ({ name, symbol });
 
 const drawGame = () => {
   const msg = 'This game is a draw!';
@@ -52,49 +39,60 @@ const render = (cells, gameBoard, index) => {
   cells[parseInt(index) - 1].innerHTML = `<p>${gameBoard.board[index - 1]}</p>`;
 };
 
-const updateCell = (gameBoard, event, winCombos, playersObj) => {
+const checkWin = (winCombos, symbol, gameBoard) => {
+  for (let i = 0; i < winCombos.length; i += 1) {
+    const a = gameBoard.board[winCombos[i][0]];
+    const b = gameBoard.board[winCombos[i][1]];
+    const c = gameBoard.board[winCombos[i][2]];
+
+    if (a === symbol && b === symbol && c === symbol) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const gameFlow = (gameBoard, event, winCombos, playersObj) => {
   const { index } = event.target.dataset;
   const gameArr = gameBoard.board;
-  // console.log(event.target.dataset);
   const countX = gameArr.filter((element) => element === 'X').length;
   const countO = gameArr.filter((element) => element === 'O').length;
-  // console.log(countO);
   const marker = countX > countO ? 'O' : 'X';
   gameArr[index - 1] = marker;
   render(cells, gameBoard, index);
-  // console.log(gameArr);
   const win = checkWin(winCombos, marker, gameBoard);
 
   const player = playersObj.filter(el => el.symbol === marker)[0].name;
+  const nextPlayer = playersObj.filter((el) => el.symbol !== marker)[0].name;
 
   if (win) {
     endGame(player);
   } else if (gameArr.every((ele) => ele !== '') && win !== true) {
     drawGame();
   } else {
-    // continue;
+    message.innerHTML = `<p>${nextPlayer}, it's your turn...</p>`;
   }
 };
-
-
-const createPlayer = (name, symbol) => ({ name, symbol });
 
 const startGame = (e) => {
   e.preventDefault();
   playersArr.push(playerName.value);
+  form.reset();
+  message.innerHTML = '<p>Player two, enter name...</p>';
   if (playersArr.length === 2) {
+    message.innerHTML = `<p>${playersArr[0]}, it's your turn...</p>`;
+    form.classList.add('hide');
+    boardDisplay.classList.remove('hide');
     const playerOne = createPlayer(playersArr[0], playerOneSym);
     const playerTwo = createPlayer(playersArr[1], playerTwoSym);
     playersObj.push(playerOne);
     playersObj.push(playerTwo);
   }
-  form.reset();
 };
-
 
 [].forEach.call(cells, (e) => {
   e.addEventListener('click', (e) => {
-    updateCell(
+    gameFlow(
       gameBoard,
       e,
       winCombos,
@@ -102,6 +100,5 @@ const startGame = (e) => {
     );
   }, { once: true });
 });
-
 
 form.addEventListener('submit', (e) => startGame(e));
